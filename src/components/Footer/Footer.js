fd9c2ad5ab7aafc4441739878bc45813bb31b931
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-05-10 20:53:40 -0700

    Create Footer component skeleton

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Footer.scss';
+
+Footer.propTypes = {
+
+};
+
+function Footer(props) {
+  return (
+    <footer className={styles.Footer}>
+      Footer
+    </footer>
+  );
+};
+
+export default Footer;
