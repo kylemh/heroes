452bcdeea00aebe39bc9e48dd208e9229517c459
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-05-10 22:42:11 -0700

    Dynamically display some data

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import styles from './App.scss';
+import characterData from 'api/characterData';
 import Header from 'components/Header/Header';
-import Button from 'components/Button/Button';
 import logo from 'images/logo.svg';
 
 class App extends Component {
-  render() {
-    const styles = require('./App.scss');
+  state = {
+    characters: [...characterData]
+  }
 
+  // playAudio = audio => {
+  //   const characterAudio = new Audio(audio);
+  //   characterAudio.play();
+  // }
+
+  render() {
     return (
       <div className={styles.App}>
         <Header logo={logo} title="Heroes (ft. React)" />
         <div className={styles.content}>
-          <Button>
-            Click me!
-          </Button>
+          {characterData.map(({ audio, hp, image, name }) => {
+            // const characterAudio = require(audio);
+            // const characterImage = require(image);
+
+            return (
+              <div
+                className={styles.characterCard}
+                key={name}
+                // onClick={() => this.playAudio(characterAudio)}
+              >
+                <h3>{name}</h3>
+                {/* <img src={characterImage} alt={`Portrait of ${name}`} /> */}
+                <span>{hp}/{hp}&nbsp;</span>
+                <progress value={hp} max={hp} />
+              </div>
+            )
+          })}
         </div>
       </div>
     );
