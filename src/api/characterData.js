4c72161726007a2312c10f40dd70b73fb05617b2
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-05-10 22:40:50 -0700

    Fix typos in data

--- src/api/characterData.js
+++ src/api/characterData.js
@@ -1,30 +1,30 @@
 export default [
   {
-      name: 'Chewbaca',
-      hp: 120,
-      strength: 5,
-      image: 'images/chewie.jpg',
-      audio: 'sounds/chewie.mp3'
+    name: 'Chewbaca',
+    hp: 120,
+    strength: 5,
+    image: 'images/chewie.jpg',
+    audio: 'audio/chewie.mp3'
   },
   {
-      name: 'Darth Vader',
-      hp: 150,
-      strength: 7,
-      image: 'images/darth.png',
-      audio: 'sounds/darthbreath.mp3'
+    name: 'Darth Vader',
+    hp: 150,
+    strength: 7,
+    image: 'images/darth.png',
+    audio: 'audio/darthbreath.mp3'
   },
   {
-      name: 'Princess Leia',
-      hp: 160,
-      strength: 8,
-      image: 'images/leia.jpg',
-      audio: 'sounds/leiasound.mp3'
+    name: 'Princess Leia',
+    hp: 160,
+    strength: 8,
+    image: 'images/leia.jpg',
+    audio: 'audio/leiasound.mp3'
   },
   {
-      name: 'Obi Wan Kenobi',
-      hp: 115,
-      strength: 6,
-      image: 'images/obiwan.png',
-      audio: 'sounds/obisound.mp3'
+    name: 'Obi Wan Kenobi',
+    hp: 115,
+    strength: 6,
+    image: 'images/obiwan.png',
+    audio: 'audio/obisound.mp3'
   },
 ];
