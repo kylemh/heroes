608ed0a7743ea4464526c880b9e254cce61a12c3
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-05-18 02:23:09 -0700

    Fix bug in data

--- src/api/characterData.js
+++ src/api/characterData.js
@@ -1,30 +1,31 @@
+/* eslint-disable no-undef */
 export default [
   {
     name: 'Chewbaca',
     hp: 120,
     strength: 5,
     image: require('images/chewie.jpg'),
-    audio: 'audio/chewie.mp3'
+    audio: require('audio/chewie.mp3'),
   },
   {
     name: 'Darth Vader',
     hp: 150,
     strength: 7,
     image: require('images/darth.png'),
-    audio: require('audio/darthbreath.mp3')
+    audio: require('audio/darthbreath.mp3'),
   },
   {
     name: 'Princess Leia',
     hp: 160,
     strength: 8,
     image: require('images/leia.jpg'),
-    audio: require('audio/leiasound.mp3')
+    audio: require('audio/leiasound.mp3'),
   },
   {
     name: 'Obi Wan Kenobi',
     hp: 115,
     strength: 6,
     image: require('images/obiwan.png'),
-    audio: require('audio/obisound.mp3')
-  },
+    audio: require('audio/obisound.mp3'),
+  }
 ];
