074c011bc0fddb79a55fdc40cf7b944dffefd728
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-05-27 19:13:40 -0700

    Move audio function to Character container

--- src/containers/Character/Character.js
+++ src/containers/Character/Character.js
@@ -7,11 +7,17 @@ import styles from './Character.scss';
 class Character extends Component {
   static propTypes = {
     audio: PropTypes.string.isRequired,
+    className: PropTypes.string,
     hp: PropTypes.number.isRequired,
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
+    onClick: PropTypes.func,
     strength: PropTypes.number.isRequired,
+  };
+
+  static defaultProps = {
+    className: undefined,
+    onClick: () => {},
   };
 
   state = {
@@ -21,19 +27,30 @@ class Character extends Component {
   };
 
   characterClickHandler = () => {
-    if (this.state.isAlive && !this.state.selected) {
-      this.setState({ selected: true }, this.props.onClick());
+    const { props, state } = this;
+
+    if (state.isAlive && !state.selected) {
+      this.setState({ selected: true }, () => {
+        this.playAudio(props.audio);
+        props.onClick();
+      });
     }
 
-    if (this.state.selected) {
+    if (state.selected) {
       // eslint-disable-next-line no-undef
       window.alert('This character has already been selected.');
     }
 
-    if (!this.state.isAlive) {
+    if (!state.isAlive) {
       // eslint-disable-next-line no-undef
       window.alert('This character is dead... Select another.');
     }
+  };
+
+  playAudio = audio => {
+    // eslint-disable-next-line no-undef
+    const characterAudio = new Audio(audio);
+    characterAudio.play();
   };
 
   render() {
@@ -41,7 +58,7 @@ class Character extends Component {
 
     return (
       <Card
-        className={classNames(styles.Character, {
+        className={classNames(styles.Character, props.className, {
           [styles.dead]: !state.isAlive,
           [styles.selected]: state.selected,
         })}
